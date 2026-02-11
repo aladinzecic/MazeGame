9289f102a87fb1864c923b78a168ae2e1ffff5c5
aladinzecic <aladinzecic10@gmail.com>
2026-02-11 01:20:12 +0100

    pick your character

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,29 +1,43 @@
-import React from 'react'
-import "./LandingPage.css"
-import btn from "../../assets/dugme.png"
+import React, { useState } from "react";
+import "./LandingPage.css";
+import btn from "../../assets/dugme.png";
+import kec from "../../assets/kec.png";
+import dva from "../../assets/dva.png";
+import tri from "../../assets/tri.png";
+import cetri from "../../assets/cetri.png";
+
 export default function LandingPage() {
+  const [zoomed, setZoomed] = useState(false);
+
   return (
-    <div className="full">
-      <svg
-        className="curve-text"
-        width="600"
-        height="400"
-        viewBox="0 0 600 1"
-      >
+    <div className={`full ${zoomed ? "zoomed" : ""}`}>
+      <svg className={`curve-text ${zoomed ? "hidden" : ""}`}  width="600" height="400" viewBox="0 0 600 1">
         <path
           id="arc"
           d="M 80 150 A 320 320 0 0 1 520 150"
           fill="transparent"
         />
         <text>
-          <textPath href="#arc" startOffset="50%" textAnchor="middle">
+          <textPath href="#arc" startOffset="50%" textAnchor="middle" >
             Escape the jungle
           </textPath>
         </text>
       </svg>
-      <div className="btn-full">
-      <img className="btn-img" src={btn} />
-      <h1 className="btn-text">PLAY</h1>
+
+      <div className={`btn-full ${zoomed ? "hidden" : ""}`}>
+        <img className="btn-img" src={btn} alt="Play button" />
+        <h1
+          className="btn-text"
+          onClick={() => setZoomed(true)}
+        >
+          PLAY
+        </h1>
+      </div>
+      <div className="characters">
+        <img className="" src={kec} />
+        <img className="" src={dva} />
+        <img className="" src={tri} />
+        <img className="" src={cetri} />
       </div>
     </div>
   );
